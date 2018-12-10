ff39709644d7751b9078b55822d6de1f814c76d7
Ivonne Lawless <ivonnesemail@gmail.com>
2018-12-10 01:12:51 -0500

    added pseudocode and array

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// * Pseudo Code * //
+
+//Create an array of 'Worst Jelly Bean Flavors of All Time!'
+const worstFlavors = ["old van", "bones", "hair", "gasoline", "ryan reynolds", "boiled ice", "lightbulbs"];
+//Get computer choice
+let underscoreCount = Math.floor(Math.random() * worstFlavors.length);
+console.log(underscoreCount);
+//Convert to underscore
+//Grab users choice
+//Compare computer vs user choice
+//If correct push to correct
+//If incorrect push to incorrect
+
